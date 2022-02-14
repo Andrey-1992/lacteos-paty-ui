78e48b3bccfc2c6ca28144d3da7e06d05b6caf6b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-14 16:32:22 -0700

    Add type compoent for App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Lacteos Paty</h1>

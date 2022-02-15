595498ba6b46d7e74f6499279da0c023ff49947b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-14 17:10:40 -0700

    Creagte components format for the re usable components

---
+++ src/components/Fabrica/AdminFab/MenuFabAdmin/MenuFabAdmin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MenuFabAdmin.css';
+
+export const MenuFabAdmin: React.FC = () => {
+  return (
+    <div className="MenuFabAdmin">
+      <p>MenuFabAdmin</p>
+    </div>
+  );
+};
---
+++ src/components/Fabrica/ProdFab/MenuFabProd/ProduccionFabProd/ProduccionFabProd.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProduccionFabProd.css';
+
+export const ProduccionFabProd: React.FC = () => {
+  return (
+    <div className="ProduccionFabProd">
+      <p>ProduccionFabProd</p>
+    </div>
+  );
+};

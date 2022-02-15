09150c829bc1ad202ff09f477f62ae8ea98a0381
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-14 17:00:29 -0700

    Set up fabrica menu for the main compknet

---
+++ src/components/Fabrica/Fabrica.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Fabrica.css';
+
+export const Fabrica: React.FC = () => {
+  return (
+    <div className="fabrica">
+      <p>Fabrica</p>
+    </div>
+  );
+};

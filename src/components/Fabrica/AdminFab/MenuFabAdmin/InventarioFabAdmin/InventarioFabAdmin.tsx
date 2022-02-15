216851fe0c58060d7da2e353560cf415c90fb403
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-14 17:05:04 -0700

    Set up basic compnent format for the fab admin files

---
+++ src/components/Fabrica/AdminFab/MenuFabAdmin/InventarioFabAdmin/InventarioFabAdmin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './InventarioFabAdmin.css';
+
+export const InventarioFabAdmin: React.FC = () => {
+  return (
+    <div className="InventarioFabAdmin">
+      <p>InventarioFabAdmin</p>
+    </div>
+  );
+};
---
+++ src/components/Fabrica/AdminFab/MenuFabAdmin/ProduccionFabAdmin/ProduccionFabAdmin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProduccionFabAdmin.css';
+
+export const ProduccionFabAdmin: React.FC = () => {
+  return (
+    <div className="ProduccionFabAdmin">
+      <p>ProduccionFabAdmin</p>
+    </div>
+  );
+};

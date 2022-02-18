bbd89b0cfb451d2af68bda009a977299a6626055
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-17 23:21:31 -0700

    Add routes with routes and links

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { MenuSelectionI } from '../Fabrica/FabReusableComps/MenuSelectionI/MenuSelectionI';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <h1>Lacteos Paty (App Component)</h1> */}
-      <h2 className={'menu-text'}>Distribucion</h2>
-      <h2 className={'menu-text'}>Fabrica</h2>
+      <Routes>
+        <Route 
+          path='/'
+          element={
+            <div className={'main-menu-selection'}>
+              <h2 className={'menu-text'}>Distribucion</h2>
+              <h2 className={'menu-text'}>Fabrica</h2>
+            </div>
+          }
+          />
+        </Routes>
       {/* <MenuSelectionI firstSection={'Distribucion'} secondSection={'Fabrica'} /> */}
     </div>
   );

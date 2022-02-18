d1aeabf13f40c6ebcd4cebd304805ebd5798915c
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-02-17 22:50:15 -0700

    Add basic styloing to the font of the main menu

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,8 +5,10 @@ import { MenuSelectionI } from '../Fabrica/FabReusableComps/MenuSelectionI/MenuS
 export const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Lacteos Paty (App Component)</h1>
-      <MenuSelectionI firstSection={'Distribucion'} secondSection={'Fabrica'} />
+      {/* <h1>Lacteos Paty (App Component)</h1> */}
+      <h2 className={'menu-text'}>Distribucion</h2>
+      <h2 className={'menu-text'}>Fabrica</h2>
+      {/* <MenuSelectionI firstSection={'Distribucion'} secondSection={'Fabrica'} /> */}
     </div>
   );
 };
--- src/components/Fabrica/Fabrica.tsx
+++ src/components/Fabrica/Fabrica.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { MenuSelectionI } from '../Fabrica/FabReusableComps/MenuSelectionI/MenuSelectionI';
 import './Fabrica.css';
 
 export const Fabrica: React.FC = () => {
   return (
     <div className="fabrica">
-      <p>Fabrica</p>
+      <h2 className={'menu-text'}>Administracion Fabrica</h2>
+      <h2 className={'menu-text'}>Produccion Fabrica</h2>
+      {/* <MenuSelectionI firstSection={'Administracion Fabrica'} secondSection={'Produccion Fabrica'} /> */}
     </div>
   );
 };
